e669c2124864acad4e40b1dc381264f08daaffd6
Jan Gasser <jan.gasser@vzug.com>
2016-09-27 16:18:39 +0200

    Created Views, added depencencies, added capture method

--- Memory/app/src/main/java/com/vzug/memory/MainActivity.java
+++ Memory/app/src/main/java/com/vzug/memory/MainActivity.java
@@ -3,6 +3,9 @@ package com.vzug.memory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+
 }
