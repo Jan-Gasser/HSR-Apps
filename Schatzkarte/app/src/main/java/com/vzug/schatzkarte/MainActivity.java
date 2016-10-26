78682dbc3c1a399745781a6989833ba18ac90252
Jan-Gasser <jan.gasser@bluewin.ch>
2016-10-26 19:32:53 +0200

    Permission for SDK > 22 Creating map at start

--- Schatzkarte/app/src/main/java/com/vzug/schatzkarte/MainActivity.java
+++ Schatzkarte/app/src/main/java/com/vzug/schatzkarte/MainActivity.java
@@ -1,16 +1,27 @@
 package com.vzug.schatzkarte;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.MapTileProviderBase;
 import org.osmdroid.tileprovider.modules.IArchiveFile;
 import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
 
 import java.io.File;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +30,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] perms = {android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.ACCESS_FINE_LOCATION};
+        if (Build.VERSION.SDK_INT > 22 && ContextCompat.checkSelfPermission(this,perms[0]) == PackageManager.PERMISSION_DENIED
+                && ContextCompat.checkSelfPermission(this,perms[1]) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,perms,1);
+        }
+        else {
+            createMap();
+        }
+    }
 
-        createMap();
+    @Override
+    public void onRequestPermissionsResult(int requestcode, String[] perms, int[] grantResults) {
+        if (ContextCompat.checkSelfPermission(this,perms[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,perms[1]) == PackageManager.PERMISSION_GRANTED){
+            createMap();
+        }
     }
 
     private void createMap() {
@@ -28,14 +54,18 @@ public class MainActivity extends AppCompatActivity {
 
         SimpleRegisterReceiver simpleRegisterReceiver = new SimpleRegisterReceiver(this);
         XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", 1, 20, 256, ".png", new String[]{ });
-        mapview.setTileSource(treasureMapTileSource);
+        mapview.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         mapview.setMultiTouchControls(true);
         mapview.setBuiltInZoomControls(true);
         mapview.getController().setZoom(18);
 
         File file = new File(Environment.getExternalStorageDirectory(), "osmdroid/hsr.mbtiles");
         //Crasht hier
-        //IArchiveFile[] files = {MBTilesFileArchive.getDatabaseFileArchive(file)};
+        IArchiveFile[] files = { MBTilesFileArchive.getDatabaseFileArchive(file)};
+        MapTileModuleProviderBase treasureMapProvbase = new MapTileFileArchiveProvider(simpleRegisterReceiver,treasureMapTileSource, files);
+        MapTileProviderBase mtproviderbase = new MapTileProviderArray(treasureMapTileSource,null,new MapTileModuleProviderBase[] {treasureMapProvbase});
+        TilesOverlay treasuremapTilesOverlay = new TilesOverlay(mtproviderbase, getBaseContext());
+        treasuremapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
+        mapview.getOverlays().add(treasuremapTilesOverlay);
     }
-
 }
